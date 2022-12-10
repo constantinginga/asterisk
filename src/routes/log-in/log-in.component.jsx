72b134eec72e30b27d5fb2069be2d69144b8d9ee
Constantin Ginga <costelgn@gmail.com>
2022-12-10 15:37:18 +0100

    Add login with redirect

--- src/routes/log-in/log-in.component.jsx
+++ src/routes/log-in/log-in.component.jsx
@@ -1,18 +1,33 @@
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
 const LogIn = () => {
+  // for logging in with redirect
+  useEffect(async () => {
+    const response = await getRedirectResult(auth);
+    if (response) {
+      const userDocRef = await createUserDocumentFromAuth(response.user);
+    }
+  }, []);
+
   const logInGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(response.user);
-    console.log(response);
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
-      <button onClick={logInGoogleUser}>Log in with Google</button>
+      <button onClick={logInGoogleUser}>Log in with Google popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Log in with Google redirect
+      </button>
     </div>
   );
 };
