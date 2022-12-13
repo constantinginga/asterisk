0e00d9674c60ff7217df00874aac94c55bf83c44
Constantin Ginga <costelgn@gmail.com>
2022-12-13 16:06:05 +0100

    Add CartIcon component & CartDropdown component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { logOut } from '../../utils/firebase/firebase.utils';
 import { UserContext } from '../../contexts/user.context';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -29,7 +31,9 @@ const Navigation = () => {
               Log In
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
