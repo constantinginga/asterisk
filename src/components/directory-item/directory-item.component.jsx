d5127a5bff622a318fb388a62266d9ca5b04d466
Constantin Ginga <costelgn@gmail.com>
2022-12-20 13:11:40 +0200

    Add routing to frontpage items

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import {
   BackgroundImage,
   Body,
   DirectoryItemContainer,
 } from './directory-item.styles';
 
-const DirectoryItem = ({ item: { imageUrl, title } }) => (
-  <DirectoryItemContainer>
-    <BackgroundImage imageUrl={imageUrl} />
-    <Body>
-      <h2>{title}</h2>
-      <p>Shop Now</p>
-    </Body>
-  </DirectoryItemContainer>
-);
+const DirectoryItem = ({ item: { imageUrl, title, route } }) => {
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
+
+  return (
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <Body>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </Body>
+    </DirectoryItemContainer>
+  );
+};
 
 export default DirectoryItem;
