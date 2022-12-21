948ccefc3e4c6b2ed67f79e843a4c45895833b34
Constantin Ginga <costelgn@gmail.com>
2022-12-21 14:05:01 +0200

    Convert UserContext to Reducers

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangedListener,
@@ -11,8 +11,38 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTIONS = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+  console.log('dispatched');
+  console.log(action);
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTIONS.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Invalid action type ${type} in userReducer`);
+  }
+};
+
+export const INITIAL_STATE = {
+  currentUser: null,
+};
+
 // component that will wrap around any component that needs access to the context
 export const UserProvider = ({ children }) => {
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  console.log(currentUser);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTIONS.SET_CURRENT_USER, payload: user });
+  };
+
   // run once, when the component mounts
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(async (user) => {
@@ -22,7 +52,7 @@ export const UserProvider = ({ children }) => {
 
     return unsubscribe;
   }, []);
-  const [currentUser, setCurrentUser] = useState(null);
+
   const value = { currentUser, setCurrentUser };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
