339845f226acb60f22b316fd86f00c625d8e1707
Constantin Ginga <costelgn@gmail.com>
2023-01-15 16:25:49 +0100

    Convert categories thunk to saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,25 +1,11 @@
 import { createAction } from '../../utils/reducer/reducer.utils';
 import { CATEGORY_ACTIONS } from './category.types';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
-const fetchCategoriesStart = () =>
+export const fetchCategoriesStart = () =>
   createAction(CATEGORY_ACTIONS.FETCH_CATEGORIES_START);
 
-const fetchCategoriesSuccess = (categories) =>
+export const fetchCategoriesSuccess = (categories) =>
   createAction(CATEGORY_ACTIONS.FETCH_CATEGORIES_SUCCESS, categories);
 
-const fetchCategoriesFailure = (error) =>
+export const fetchCategoriesFailure = (error) =>
   createAction(CATEGORY_ACTIONS.FETCH_CATEGORIES_FAILURE, error);
-
-// thunk action
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-
-  try {
-    const categories = await getCategoriesAndDocuments();
-    dispatch(fetchCategoriesSuccess(categories));
-  } catch (error) {
-    dispatch(fetchCategoriesFailure(error));
-  }
-};
