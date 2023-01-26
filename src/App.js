e3f8de7838e4f2571fad906af10a573f12706fdf
Constantin Ginga <costelgn@gmail.com>
2023-01-26 13:08:40 +0100

    Implement dynamic imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-
+import Spinner from './components/spinner/spinner.component';
 import { checkUserSession } from './store/user/user.action';
 
-import Navigation from './routes/navigation/navigation.component';
-import Home from './routes/home/home.component';
-import Auth from './routes/auth/auth.component';
-import Shop from './routes/shop/shop.component';
-import Checkout from './routes/checkout/checkout.component';
+const Shop = lazy(() => import('./routes/shop/shop.component'));
+const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
+const Home = lazy(() => import('./routes/home/home.component'));
+const Auth = lazy(() => import('./routes/auth/auth.component'));
+const Navigation = lazy(() =>
+  import('./routes/navigation/navigation.component')
+);
 
 const App = () => {
   const dispatch = useDispatch();
@@ -16,17 +18,19 @@ const App = () => {
   // run once, when the component mounts
   useEffect(() => {
     dispatch(checkUserSession());
-  }, []);
+  }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="auth" element={<Auth />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="auth" element={<Auth />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
