5ddcfd1e00e87c5bc1d370b12f7bc16b83a55fed
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-12-02 22:16:10 +0100

    .

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -1,37 +1,35 @@
-// require('dotenv').config();
-// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-// exports.handler = async (request, response) => {
-//   try {
-//     const { amount } = JSON.parse(request.body);
-//     const paymentIntent = await stripe.paymentIntents.create({
-//       amount,
-//       currency: 'usd',
-//       automatic_payment_methods: {
-//         enabled: true,
-//       },
-//     });
+exports.handler = async (request, response) => {
+  try {
+    const { amount } = JSON.parse(request.body);
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
 
-//     response.status(200).json({
-//       query: request.query,
-//       cookies: request.cookies,
-//       body: JSON.stringify({ paymentIntent }),
-//     });
-//   } catch (error) {
-//     console.log({ error });
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ paymentIntent }),
+    }
+  } catch (error) {
+    console.log({ error });
 
-//     response.status(500).json({
-//       query: request.query,
-//       cookies: request.cookies,
-//       body: JSON.stringify({ error: error.message }),
-//     });
-//   }
-// }
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    }
+  }
+}
 
-exports.handler = async function (event, context, callback) {
-  return {
-    statusCode: 200,
-    headers: {},
-    body: 'Hello world',
-  };
-};
+// exports.handler = async function (event, context, callback) {
+//   return {
+//     statusCode: 200,
+//     headers: {},
+//     body: 'Hello world',
+//   };
+// };
